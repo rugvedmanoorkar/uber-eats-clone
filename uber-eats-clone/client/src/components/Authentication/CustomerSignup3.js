6da62ea49ddbf9257127e399e97b41994be950b8
rugvedmanoorkar <manoorkar.rugved@gmail.com>
2021-10-09 00:33:15 -0700

    added customersignup new form

---
+++ uber-eats-clone/client/src/components/Authentication/CustomerSignup3.js
@@ -0,0 +1,385 @@
+import React, { useState } from "react";
+
+
+const CustomerSignup3 = () => {
+  const [fullname, setFullname] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [rpassword, setRpassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [about, setAbout] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [country, setCountry ] = useState(""); 
+  const [pin, setPin ] = useState(0); 
+  const [dob, setDob ] = useState(0); 
+
+  
+
+
+  return (
+    <div>
+      <div class="cs-main">
+        <div className="cs-main2">
+          <div class="cs-main-heading">Sign Up with Uber</div>
+          <div class="cs-main-hidden"></div>
+        </div>
+
+        <div className="cs-form">
+          <form className="cs-form2" method="post">
+            <div>
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  Full name (required){" "}
+                </label>
+
+                <div class="cs-input">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    value={fullname}
+                    class="cs-input-box"
+                    placeholder="Full name"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setFullname(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  User name (required){" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    value={username}
+                    class="cs-input-box"
+                    placeholder="Username"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  Password (required){" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="password"
+                    value={password}
+                    class="cs-input-box"
+                    placeholder="Password"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  Repeat password (required){" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="password"
+                    value={rpassword}
+                    class="cs-input-box"
+                    placeholder="Password"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setRpassword(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;
+                  {password == rpassword ? "" : "Please re-enter same password  "}
+                  </div>
+                </div>
+              </div>
+
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  Email{" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    value={email}
+                    class="cs-input-box"
+                    placeholder="Email"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  City{" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    value={city}
+                    class="cs-input-box"
+                    placeholder="City"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setCity(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  State{" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    value={state}
+                    class="cs-input-box"
+                    placeholder="State"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setState(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              {/* Country */}
+              
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  Country{" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    value={country}
+                    class="cs-input-box"
+                    placeholder="Full name"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setCountry(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              {/* Pin */}
+
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  Pin{" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="pincode"
+                    name="pincode"
+                    type="number"
+                    value={pin}
+                    class="cs-input-box"
+                    placeholder=""
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    min="10000"
+                    onChange={(e) => {
+                        setPin(e.target.value);
+                      }}
+                    
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  About{" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    value={about}
+                    class="cs-input-box"
+                    placeholder="Full name"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setAbout(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              <div class="cs-input-text">
+                <label id="cs-input-label" class="cs-input-label" for="Name">
+                  {" "}
+                  Nickname{" "}
+                </label>
+                <div class="cs-input">
+                  <input
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    value={nickname}
+                    class="cs-input-box"
+                    placeholder="Full name"
+                    aria-describedby="error-caption-cs-input-label form-title cs-input-label"
+                    aria-required="true"
+                    autocorrect="on"
+                    autocapitalize="words"
+                    placeholdermsgkey="m2.first_name"
+                    autofocus="on"
+                    onChange={(e) => {
+                        setNickname(e.target.value);
+                      }}
+                  />
+                  <div class="cs-input-icon"></div>
+                </div>
+                <div role="alert" aria-live="assertive" aria-atomic="false">
+                  <div class="cs-input-alert">&nbsp;</div>
+                </div>
+              </div>
+
+              <div className="cl-next-btn">
+                <button className="cl-next-btn2" >
+                  <span className="cl-next-btn-label">Signup</span>
+                  <i className="cl-next-btn-icon"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerSignup3;
